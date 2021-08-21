23651d64e02f0617a1faebae9045b1ab7c4270aa
AkbarZul <akbar.zul73@gmail.com>
2021-08-21 08:46:24 +0700

    add package and component navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// import react from "react";
+import NavbarComp from "./components/NavbarComp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> tester doang cuk 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComp />
+      <h2>Hallo</h2>
     </div>
   );
 }
